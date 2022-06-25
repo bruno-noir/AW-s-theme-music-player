f73cf78b2cc86618253686fae18bb7e8fbabb589
Pratyush Kumar Chouhan <98751013+bruno-noir@users.noreply.github.com>
2022-06-25 21:01:42 +0530

    added files

---
+++ js/music-list.js
@@ -0,0 +1,106 @@
+let allMusic = [
+
+    {
+        name: "Somebody Like U",
+        artist: "Alan Walker",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Blue",
+        artist: "Alan Walker",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "The Drum",
+        artist: "Alan Walker",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Headlights",
+        artist: "Alan Walker",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Not You",
+        artist: "Alan Walker",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "OK",
+        artist: "Alan Walker",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Alan Walker - Out Of Love",
+        artist: "Alan Walker",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "World We Used To Know",
+        artist: "Alan Walker",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Paradise",
+        artist: "Alan Walker",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Running Out Of Roses",
+        artist: "Alan Walker",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Alone, Pt. II",
+        artist: "Alan Walker",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Belivers",
+        artist: "Alan Walker",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Man On The Moon",
+        artist: "Alan Walker",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Fake A Smile",
+        artist: "Alan Walker",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Sorry",
+        artist: "Alan Walker",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Space Melody",
+        artist: "Alan Walker",
+        img: "music-17",
+        src: "music-17"
+    }
+    
+    ];
